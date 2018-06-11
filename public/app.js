ce55d4665fc99edfa59362e037ac6184570c7852
Paul Kelly <paul.kelly20@gmail.com>
2018-06-11 19:48:06 +0100

    refactored to add all cats in js function

--- public/app.js
+++ public/app.js
@@ -1,4 +1,13 @@
 var app = function() {
+
+  max = {name: "Max",favouriteFood: "Whiskas Temptations", img: "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg"}
+  barnaby = {name: "Barnaby",favouriteFood: "Tuna", img: "https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg"}
+  boba = {name: "Boba",favouriteFood: "Sock fluff", img: "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg"}
+
+  cats = [max,barnaby, boba]
+  for(cat of cats){
+    addCat(cat.name, cat.favouriteFood, cat.img)
+  }
   addCat("Sam", "Salmon", 'https://i.ytimg.com/vi/YCaGYUIfdy4/maxresdefault.jpg');
 };
 
@@ -41,4 +50,4 @@ var appendElements = function(catName, catFood, catImg, ul, li){
   ul.appendChild(li);
 };
 
-window.onclick = app;
+window.onload = app;
